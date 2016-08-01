2a10822c4cbb7364fe0432d59ebe6b593442af31
Thibault <thibault.chevrin@navendis.com>
2016-08-01 17:04:42 +0200

    Update to cli beta.10

--- src/app/board/focus/focus.component.spec.ts
+++ src/app/board/focus/focus.component.spec.ts
@@ -2,19 +2,24 @@
 
 import { By }           from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { addProviders, inject } from '@angular/core/testing';
+import { Subject } from 'rxjs';
 
-import {
-  beforeEach, beforeEachProviders,
-  describe, xdescribe,
-  expect, it, xit,
-  async, inject
-} from '@angular/core/testing';
-
+import { action, stateFn, AppState, STATE_AND_DISPATCHER } from '../../shared';
 import { FocusComponent } from './focus.component';
 
 describe('Component: Focus', () => {
-  it('should create an instance', () => {
-    let component = new FocusComponent();
-    expect(component).toBeTruthy();
-  });
+
+  const actions = new Subject<action>();
+  const initState: AppState = {
+    activities: [],
+    userStates: null
+  };
+  const states = stateFn(initState, actions);
+
+  beforeEach(() => addProviders([...STATE_AND_DISPATCHER, FocusComponent]));
+
+  it('should create an instance', inject([FocusComponent], (focus: FocusComponent) => {
+    expect(focus).toBeTruthy();
+  }));
 });
