2ae8ff1b283c9069afe038fe889769c3de51d984
Draeken <pbm764927@gmail.com>
2016-12-07 17:02:58 +0100

    Fix tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,19 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async } from '@angular/core/testing';
+
+import { RouterOutletStubComponent } from './testing/router-stubs'
 import { AppComponent } from './app.component';
 
-describe('App: AutoSchedule', () => {
+describe('AppComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        RouterOutletStubComponent
       ],
     });
+    TestBed.compileComponents();
   });
 
   it('should create the app', async(() => {
@@ -18,4 +22,16 @@ describe('App: AutoSchedule', () => {
     expect(app).toBeTruthy();
   }));
 
+  it(`should have as title 'AutoSchedule'`, async(() => {
+    let fixture = TestBed.createComponent(AppComponent);
+    let app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('AutoSchedule');
+  }));
+
+  it('should render title in a h1 tag', async(() => {
+    let fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    let compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('AutoSchedule');
+  }));
 });
