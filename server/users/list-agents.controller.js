732e37d0fa62ef878c63d850a6959b96fe4231ca
Draeken <pbm764927@gmail.com>
2017-02-17 16:25:23 +0100

    Limit data sent with list-agent
    Get only url, description and name of agents

--- server/users/list-agents.controller.js
+++ server/users/list-agents.controller.js
@@ -2,7 +2,7 @@ var Agent = require('../agents/agent.model');
 
 module.exports = (options) => {
   return (req, res, next) => {
-    Agent.find().exec()
+    Agent.find().select('-_id url description name').exec()
       .then(agents => res.json(agents))
       .catch(next);
   }
